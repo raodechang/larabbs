c859adc1ec8e30f501d705b0a1d7e8c43e2521bd
raodechang <raodechang2012@163.com>
2019-06-27 02:56:13 +0000

    上传头像

--- database/migrations/2019_06_26_134624_add_avatar_and_introduction_to_users_table.php
+++ database/migrations/2019_06_26_134624_add_avatar_and_introduction_to_users_table.php
@@ -14,7 +14,7 @@ class AddAvatarAndIntroductionToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avater')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('introduction')->nullable();
         });
     }
@@ -27,7 +27,7 @@ class AddAvatarAndIntroductionToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('avater');
+            $table->dropColumn('avatar');
             $table->dropColumn('introduction');
         });
     }
