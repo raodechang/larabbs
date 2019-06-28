cf29d69a4414cbcb1920bce47cb84a9bf476a9d8
raodechang <raodechang2012@163.com>
2019-06-28 03:36:22 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    $updated_at = $faker->dateTimeThisMonth();// 随机取一个月以内的时间
+    $created_at = $faker->dateTimeThisMonth($updated_at);// 传参为生成最大时间不超过，因为创建时间需永远比更改时间要早
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at
     ];
 });
