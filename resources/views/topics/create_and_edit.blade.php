bb2c399b6868e0a9bd87ba91f32277d5a53ffa81
raodechang <raodechang2012@163.com>
2019-07-09 02:59:22 +0000

    WYSIWYG 编辑器

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -18,12 +18,12 @@
 
           <hr>
 
-          @if($topic->id)
-            <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
-              <input type="hidden" name="_method" value="PUT">
+              @if($topic->id)
+                <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
+                <input type="hidden" name="_method" value="PUT">
               @else
                 <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
-                  @endif
+              @endif
 
                   <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
@@ -56,3 +56,22 @@
   </div>
 
 @endsection
+
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script>
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
+@stop
