6765c943f4f7e57c4d3fe6347d3ace4fea4b05e7
raodechang <raodechang2012@163.com>
2019-07-10 16:03:29 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -17,10 +18,13 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         //第一种方法
-        $reply->topic->increment('reply_count', 1);
+        //$reply->topic->increment('reply_count', 1);
         //第二种方法
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
